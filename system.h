ff686d8a8831922d8d518aaa919444e46bb9436a
StaryPapka <alexapod@inbox.ru>
2026-05-16 11:56:26 +0300

    пока все работает

--- system.h
+++ system.h
@@ -12,19 +12,16 @@
 #include "MDR32FxQI_ssp.h"
 
 // === ЗАТЕМ конфигурация и USB ===
-
-
-#include "MDR32FxQI_config.h"           // <-- ДО usb.h
+#include "MDR32FxQI_config.h"
 
 // === USB библиотека ===
 #include "MDR32FxQI_usb.h"
 #include "MDR32FxQI_usb_device.h"
 #include "MDR32FxQI_usb_CDC.h"
 
-// === Добавьте это перед #include "MDR32FxQI_usb_handlers.h" ===
-USB_Result USB_CDC_DummyDataReceive(uint8_t* Buffer, uint32_t Length);
 // === Обработчики USB ===
-#include "MDR32FxQI_usb_handlers.h"     // <-- Последний!
+USB_Result USB_CDC_DummyDataReceive(uint8_t* Buffer, uint32_t Length);
+#include "MDR32FxQI_usb_handlers.h"
 
 // === Функции и переменные ===
 extern volatile uint32_t ms_ticks;
@@ -37,14 +34,12 @@ void send_char(char c);
 void send_string(const char* str);
 void send_decimal(uint32_t value);
 void send_hex(uint8_t value);
-
 void send_usb(const char* str);
+void send_all(const char* str);  // <-- НОВОЕ: отправка в оба интерфейса
 
-// === Обработчик USB CDC ===
-// === Обработчик USB CDC (обязательно!) ===
+// === Обработчики USB CDC ===
 USB_Result USB_CDC_RecieveData(uint8_t* Buffer, uint32_t Length);
 USB_Result USB_CDC_GetLineCoding(uint16_t wINDEX, USB_CDC_LineCoding_TypeDef* DATA);
 USB_Result USB_CDC_SetLineCoding(uint16_t wINDEX, const USB_CDC_LineCoding_TypeDef* DATA);
-
 
 #endif // SYSTEM_H
